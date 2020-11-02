7918e8b5bc6e2e13f68e95b31467614713fbba79
dhmec <dharmoradiya@gmail.com>
2020-11-02 14:32:51 -0700

    Add New Book Class in Models; Add Properties and Constructor;

---
+++ Library_DueDate_Tracker_Day1/Library_DueDate_Tracker_Day1/Models/Book.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Library_DueDate_Tracker_Day1.Models
+{
+    public class Book
+    {
+        public int ID { get; }
+        public string Title { get; }
+        public DateTime PublicationDate { get; }
+        public DateTime CheckedOutDate { get; }
+        public DateTime DueDate { get; set; }
+        public DateTime? ReturnedDate { get; set; }
+        public string Author { get; }
+        public Book(int id, string title, string author, DateTime publicationDate, DateTime chechedOutDate)
+        {
+            ID = id;
+            Title = title;
+            Author = author;
+            PublicationDate = publicationDate;
+            CheckedOutDate = chechedOutDate;
+            DueDate = CheckedOutDate.AddDays(14);
+            ReturnedDate = null;
+
+        }
+    }
+}
