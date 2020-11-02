043f3cfb5b77188ccd13649f520b79cc7a8ec968
dhmec <dharmoradiya@gmail.com>
2020-11-02 14:37:21 -0700

    Add BookController in Controllers folder; Add  propery for List of Book object

---
+++ Library_DueDate_Tracker_Day1/Library_DueDate_Tracker_Day1/Controllers/BookController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Library_DueDate_Tracker_Day1.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Library_DueDate_Tracker_Day1.Controllers
+{
+    public class BookController : Controller
+    {
+        public static List<Book> Books = new List<Book>();
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
