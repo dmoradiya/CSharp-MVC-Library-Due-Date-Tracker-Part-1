37d0a750dc470edec08610cd58eefd31f84c0713
dhmec <dharmoradiya@gmail.com>
2020-11-02 20:50:15 -0700

    Add Details view and Add GetBookByID  Method in Controller

--- Library_DueDate_Tracker_Day1/Library_DueDate_Tracker_Day1/Controllers/BookController.cs
+++ Library_DueDate_Tracker_Day1/Library_DueDate_Tracker_Day1/Controllers/BookController.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using Library_DueDate_Tracker_Day1.Models;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.VisualBasic;
 
 namespace Library_DueDate_Tracker_Day1.Controllers
 {
@@ -25,11 +26,42 @@ namespace Library_DueDate_Tracker_Day1.Controllers
 
             return View();
         }
-       
+        public IActionResult Details(int bookId)
+        {
+            GetBookByID(bookId);
+            ViewBag.Books = Books;
+            return View();
+        }
+
+
         public static List<Book> Books = new List<Book>();
         public void CreateBook(int bookID, string title, string author, DateTime publicationDate, DateTime chechedOutDate)
         {
             Books.Add(new Book(bookID, title, author, publicationDate, chechedOutDate));
+        }
+
+        // Returns the “Book” object with the given “ID” from the “Books” list.
+        public Book GetBookByID(int bookId)
+        {
+            return Books.Where(x => x.ID == bookId).SingleOrDefault();
+        }
+
+        // Extensions are 7 days from today’s date.
+        //public Book ExtendDueDateForBookByID()
+        //{
+           
+        //}
+
+        // Sets the “ReturnedDate” of the “Book” object with the given “ID” to the current date.
+        public void ReturnBookByID()
+        {
+
+        }
+
+        // Removes the “Book” object with the given “ID” from the “Books” list.
+        public void DeleteBookByID()
+        {
+
         }
     }
 }
