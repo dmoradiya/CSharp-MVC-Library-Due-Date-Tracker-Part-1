ef7d87a7e977696a4c8fb3a65d803a8a1d64fa8b
dhmec <dharmoradiya@gmail.com>
2020-11-02 23:14:25 -0700

    Add try Catch block to dispau success msg

--- Library_DueDate_Tracker_Day1/Library_DueDate_Tracker_Day1/Controllers/BookController.cs
+++ Library_DueDate_Tracker_Day1/Library_DueDate_Tracker_Day1/Controllers/BookController.cs
@@ -23,21 +23,33 @@ namespace Library_DueDate_Tracker_Day1.Controllers
         public IActionResult Create(int bookID, string title, string author, DateTime publicationDate, DateTime chechedOutDate)
         {
             CreateBook(bookID, title, author, publicationDate, chechedOutDate);
-
+            ViewBag.Books = Books;
             return View();
         }
         public IActionResult Details(int bookId)
         {
             GetBookByID(bookId);
+            ExtendDueDateForBookByID(bookId);
             ViewBag.Books = Books;
             return View();
+        }
+        public IActionResult ExtendDueDate(int bookId)
+        {
+           
+            ExtendDueDateForBookByID(bookId);
+            
+            return RedirectToAction("Details");
         }
 
 
         public static List<Book> Books = new List<Book>();
         public void CreateBook(int bookID, string title, string author, DateTime publicationDate, DateTime chechedOutDate)
         {
-            Books.Add(new Book(bookID, title, author, publicationDate, chechedOutDate));
+            if (bookID != 0)
+            {
+                Books.Add(new Book(bookID, title, author, publicationDate, chechedOutDate));
+            }
+            
         }
 
         // Returns the “Book” object with the given “ID” from the “Books” list.
@@ -47,21 +59,23 @@ namespace Library_DueDate_Tracker_Day1.Controllers
         }
 
         // Extensions are 7 days from today’s date.
-        //public Book ExtendDueDateForBookByID()
-        //{
-           
-        //}
-
-        // Sets the “ReturnedDate” of the “Book” object with the given “ID” to the current date.
-        public void ReturnBookByID()
+        public DateTime ExtendDueDateForBookByID(int bookId)
         {
 
+            return Books.Where(x=>x.ID == bookId).Select(x=>x.DueDate.AddDays(7)).SingleOrDefault();
+        }
+
+        // Sets the “ReturnedDate” of the “Book” object with the given “ID” to the current date.
+        public void ReturnBookByID(int bookId)
+        {
+            
         }
 
         // Removes the “Book” object with the given “ID” from the “Books” list.
-        public void DeleteBookByID()
+        public void DeleteBookByID(int bookid)
         {
-
+             Books.Remove(GetBookByID(bookid));
         }
+         
     }
 }
